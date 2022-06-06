e388edc13b6ae6405811a94e82677504052c2a72
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-06-06 16:24:09 -0700

    add edit function

--- src/App.js
+++ src/App.js
@@ -2,20 +2,53 @@ import React, {useState, useEffect}from 'react'
 import Header from './components/Header'
 import BeachForm from './components/BeachForm';
 import BeachContainer from './components/BeachContainer'
-import beachData from './beachData'
-function App() {
+import BeachEditForm from './components/BeachEditForm'
+//import beachData from './beachData'
+
+const App = () => {
+  const [beaches, setBeaches] = useState([]);
+  const [beachId, setBeachId] = useState(null);
+
   useEffect(() => {
-    console.log('side effect happened')
-  })
+    fetch("http://localhost:4000/api/v1/beaches")
+      .then((resp) => resp.json())
+      .then((beaches) => setBeaches(beaches));
+  }, []);
   
+  const onAddBeach = (newBeach) => {
+    setBeachId((beaches) => [...beaches, newBeach]);
+  };
+
+  const completeEditing = () => {
+    setBeachId(null);
+  };
+
+  const enterBeachEditModeFor = (beachId) => {
+    setBeachId(beachId);
+  };
+
+  const renderForm = () => {
+    if (beachId) {
+      return (
+        <BeachEditForm
+          beachId={beachId}
+          completeEditing={completeEditing}
+        />
+      );
+    } else {
+      return <BeachForm onAddBeach={onAddBeach} />;
+    }
+  };
   return (
     <div className="App">
     <Header />
-    <BeachForm beaches={beachData}/>
-    <BeachContainer beaches={beachData} />
+    <BeachContainer beaches={beaches} />
+    {renderForm()}
+    <BeachForm beaches={beaches}/>
     </div>
   );
-}
+  }
+
 
 export default App;
 
